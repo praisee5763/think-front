d170acfedc39d8d157c6a9f7593d9b24b710f81f
Your Name <you@example.com>
2023-07-21 06:13:09 +0530

    more key props

--- components/ProductReviews.js
+++ components/ProductReviews.js
@@ -111,7 +111,7 @@ export default function ProductReviews({product}) {
               <p>No reviews :(</p>
             )}
             {reviews.length > 0 && reviews.map(review => (
-              <ReviewWrapper>
+              <ReviewWrapper key={review._id}>
                 <ReviewHeader>
                   <StarsRating size={'sm'} disabled={true} defaultHowMany={review.stars} />
                   <time>{(new Date(review.createdAt)).toLocaleString('sv-SE')}</time>
